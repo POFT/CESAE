b8049a43223b056ac1184f87593ed0b9b4f51280
POFT <pedro.men.oliveira@gmail.com>
2024-06-18 14:09:29 +0100

    update_ter

--- FichasPraticas/src/FichaPratica02/Ex_15.java
+++ FichasPraticas/src/FichaPratica02/Ex_15.java
@@ -2,13 +2,14 @@ package FichaPratica02;
 
 import java.util.Scanner;
 
-public class Ex_14 {
+public class Ex_15 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
-        //Escreva um programa que leia 3 números
+
         int a, b, c;
+        String operacao;
 
         System.out.print("Insira um número (1 a 10):");
         a = input.nextInt();
@@ -18,11 +19,14 @@ public class Ex_14 {
         c = input.nextInt();
 
 
+        System.out.println("Introduza (C) para ordem crescende ou (D) para Decrescente:");
+        operacao = input.next();
         //SE a < b && a < c
         //SE b < a && b < c
         //SE c < a && c < b
 
-
+switch (operacao){
+    case "C":
         if (a < b && a < c) {
             if (b < c) {
                 System.out.print(a + "\t" + b + "\t" + c);
@@ -44,6 +48,35 @@ public class Ex_14 {
                 System.out.print(c + "\t" + a + "\t" + b);
             }
         }
+        break;
 
+        // Decrescente
+    case "D":
+        if (a < b && a < c) {
+            if (b < c) {
+                System.out.print(c + "\t" + b + "\t" + a);
+            } else {
+                System.out.print(b + "\t" + c + "\t" + a);
+            }
+        }
+        if (b < a && b < c) {
+            if (a < c) {
+                System.out.print(c + "\t" + a + "\t" + b);
+            } else {
+                System.out.print(a + "\t" + c + "\t" + b);
+            }
+        }
+        if (c < a && c < b) {
+            if (b < a) {
+                System.out.print(a + "\t" + b + "\t" + c);
+            } else {
+                System.out.print(b + "\t" + a + "\t" + c);
+            }
+        }
+        break;
+
+
+
+    }
     }
 }
